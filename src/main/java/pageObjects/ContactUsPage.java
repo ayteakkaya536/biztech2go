3cfda876e224d891a4c61d527639b312635b5263
ayteakkaya536 <ayteakkaya@hotmail.com>
2020-06-05 11:28:55 -0700

    contact us update adding addiontal files whihc was not at teh stage area

---
+++ src/main/java/pageObjects/ContactUsPage.java
@@ -0,0 +1,84 @@
+package pageObjects;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.Mouse;
+import org.openqa.selenium.support.ui.Select;
+
+public class ContactUsPage {
+
+	
+	public WebDriver driver;
+	//NOTE= modify the page objectsbelow
+//	private By signin=By.cssSelector("a[href*='sign_in']");
+//	private By title=By.cssSelector(".text-center>h2");
+//	private By NavBar=By.cssSelector(".nav.navbar-nav.navbar-right>li>a");
+//	By dropMonth=By.id("month");
+	By contactUS=By.xpath("/html/body/div[1]/div/header[1]/div/div/div/div[2]/div/nav/ul/li[7]/a/span/span[1]/span[2]");
+	By send=By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/p[5]/input");
+	By nameEmptyError=By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/p[1]/label/span/span");
+	By emailEmptyError=By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/p[2]/label/span/span");
+	By nameBlank=By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/p[1]/label/span/input");
+	By emailBlank=By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/p[2]/label/span/input");
+	By subjectBlank=By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div/form/p[3]/label/span/input");
+	By yourMessageBlank=By.xpath("//*[@id=\"wpcf7-f6006-p5505-o1\"]/form/p[4]/label/span/textarea");
+	
+	
+	
+	
+	
+	public ContactUsPage(WebDriver driver) 
+	{
+		this.driver=driver;
+	}
+	public  void openContactNewTab() throws AWTException {
+		Actions action=new Actions(driver);
+		action.moveToElement(driver.findElement(contactUS)).contextClick().build().perform();
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyRelease(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		
+	}
+	public void switchToContactUSTab() {
+		Set<String> windowIDs=driver.getWindowHandles();
+		Iterator<String> windowIterator=windowIDs.iterator(); //initilize iterator at beginning, thus next is needed for the first one
+		String parentID=windowIterator.next();
+		String childID=windowIterator.next();
+		driver.switchTo().window(childID);
+	}
+	
+	public void  clickSend()
+	{
+		driver.findElement(send).click();
+	}
+	public String nameEmptyError() {
+		return driver.findElement(nameEmptyError).getText();
+	}
+	public String emailEmptyError() {
+		return driver.findElement(emailEmptyError).getText();
+	}
+	
+	public WebElement getNameBlank() {
+		return driver.findElement(nameBlank);
+	}
+	public WebElement getEmailBlank() {
+		return driver.findElement(emailBlank);
+	}
+	public WebElement getSubjectBlank() {
+		return driver.findElement(subjectBlank);
+	}
+	public WebElement getyourMessageBlank() {
+		return driver.findElement(yourMessageBlank);
+	}	
+}
---
+++ src/test/java/com/biztech2go/TestContactUsPage.java
@@ -0,0 +1,104 @@
+//NOTE=Modify package name
+package com.biztech2go;
+
+import java.awt.AWTException;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import pageObjects.ContactUsPage;
+import pageObjects.MainPage;
+import resources.DriverInitilization;
+
+public class TestContactUsPage extends DriverInitilization{
+	
+	public static Logger log =LogManager.getLogger(DriverInitilization.class.getName());
+	public static ContactUsPage page;
+	 
+	@BeforeTest
+	public void initialize() throws IOException, AWTException, InterruptedException
+	{
+		driver =initializeDriver();
+		driver.get(prop.getProperty("url"));
+	
+		driver.manage().window().maximize();
+		
+		
+		page=new ContactUsPage(driver);
+		page.openContactNewTab();
+		Thread.sleep(2000);
+		page.switchToContactUSTab();
+	}
+	
+	@Test
+	public void validateEmptyNameEmailError() throws IOException, InterruptedException
+	{
+		
+		page.clickSend();
+		Thread.sleep(2000);
+		Assert.assertEquals(page.nameEmptyError(), "The field is required.");
+		log.info("ContactUS page name error is verified");
+		System.out.println("passed ContactUS page name error");
+		Assert.assertEquals(page.emailEmptyError(), "The field is required.");
+		log.info("ContactUS page empty email error is verified");
+		System.out.println("passed ContactUS page empty email error");
+		
+	}
+	@Test
+	public void validateNameBlankTyping() throws IOException, InterruptedException
+	{
+		page.getNameBlank().sendKeys("AAA");
+		Assert.assertEquals(page.getNameBlank().getAttribute("value"), "AAA");
+		log.info("ContactUS page name blank typing is verified");
+		System.out.println("passed ContactUS page name blank typing");
+		page.getNameBlank().clear();
+		
+	}
+	@Test
+	public void validateEmailBlankTyping() throws IOException, InterruptedException
+	{
+		page.getEmailBlank().sendKeys("BBB");
+		Assert.assertEquals(page.getEmailBlank().getAttribute("value"), "BBB");
+		log.info("ContactUS page email blank typing is verified");
+		System.out.println("passed ContactUS page email blank typing");
+		page.getEmailBlank().clear();
+		
+	}
+	@Test
+	public void validateSubjectBlankTyping() throws IOException, InterruptedException
+	{
+		page.getSubjectBlank().sendKeys("CCC");
+		Assert.assertEquals(page.getSubjectBlank().getAttribute("value"), "CCC");
+		log.info("ContactUS page subject blank typing is verified");
+		System.out.println("passed ContactUS page subject blank typing");
+		page.getSubjectBlank().clear();
+		
+	}
+	@Test
+	public void validateMessageBlankTyping() throws IOException, InterruptedException
+	{
+		page.getyourMessageBlank().sendKeys("DDD");
+		Assert.assertEquals(page.getyourMessageBlank().getAttribute("value"), "DDD");
+		log.info("ContactUS page message blank typing is verified");
+		System.out.println("passed ContactUS page message blank typing");
+		page.getyourMessageBlank().clear();
+		
+	}
+	
+	@AfterTest
+	public void teardown()
+	{
+		
+		driver.close();
+		driver=null;
+		
+	}
+}
